5ec47614459e150e8b882a12c1acdbdfaf21669d
unknown <Emily.MaciasMoran@edu.mayk.fi>
2025-03-14 11:50:56 +0200

    painikkeen lisäys

--- palvelu.py
+++ palvelu.py
@@ -10,8 +10,10 @@ def app(environ, respond):
     respond('200 OK', [('Content-type', 'text/html; charset=utf-8')])
     yield "Hello w€rld😞!".encode('utf-8')
     polku = environ["PATH_INFO"]
-    yield "<p>mokka</p>".encode('utf-8')
+
     salanimi = polku.replace("a", "aca").replace("i","hani").replace("n", "nono")
+    yield "<p>moikka</p>".encode('utf-8')
+    yield "<form method=GET><input type=button value=paina></form".encode("utf-8")
     yield(f"Salainen nimesi on: <b>{salanimi}</b>".encode('utf-8'))
    # for key in environ: 
        # yield("%s: %s\n" % (key, environ[key])).encode('utf-8')
